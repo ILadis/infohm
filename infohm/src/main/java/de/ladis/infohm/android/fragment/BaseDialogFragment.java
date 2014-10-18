bb189991d92ed4efb517435a7704c72fe9b2ba7c
Michael Wagner <mwagn4@gmail.com>
2014-10-18 02:36:06 +0200

    Adds new base fragment for dialogs

---
+++ infohm/src/main/java/de/ladis/infohm/android/fragment/BaseDialogFragment.java
@@ -0,0 +1,34 @@
+package de.ladis.infohm.android.fragment;
+
+import butterknife.ButterKnife;
+import de.ladis.infohm.android.Application;
+import de.ladis.infohm.util.Injector;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BaseDialogFragment extends DialogFragment implements Injector {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		inject(this);
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+		ButterKnife.inject(view);
+
+		return view;
+	}
+
+	@Override
+	public void inject(Object object) {
+		Application app = (Application) getActivity().getApplication();
+		app.inject(object);
+	}
+
+}

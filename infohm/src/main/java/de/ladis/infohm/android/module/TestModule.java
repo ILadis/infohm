ee8a68137181e6fd48d44f455a52984d6a1cb2a2
Michael Wagner <mwagn4@gmail.com>
2014-10-21 22:58:22 +0200

    Removes accidentally commited test files
    Removed TestActivity and TestModule classes from version control.

--- infohm/src/main/java/de/ladis/infohm/android/module/TestModule.java
+++
@@ -1,8 +0,0 @@
-package de.ladis.infohm.android.module;
-
-import dagger.Module;
-import de.ladis.infohm.android.activity.TestActivity;
-
-@Module(library = true, includes = AndroidModule.class, injects = {TestActivity.class})
-public class TestModule {
-}

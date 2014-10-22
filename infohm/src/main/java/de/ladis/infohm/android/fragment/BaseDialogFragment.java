4e874c763edf8ea70a6c924176f202fb55f6d325
Michael Wagner <mwagn4@gmail.com>
2014-10-22 20:10:43 +0200

    Updates base dialog fragment to support title and action buttons
    Edited dialog background 9-patch borders to fit the dialog's width.
    Added dialog divider background. Added custom ScrollView that supports a
    listener to notify clients of scrollable content.

--- infohm/src/main/java/de/ladis/infohm/android/fragment/BaseDialogFragment.java
+++ infohm/src/main/java/de/ladis/infohm/android/fragment/BaseDialogFragment.java
@@ -7,16 +7,21 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import butterknife.ButterKnife;
 import de.ladis.infohm.R;
 import de.ladis.infohm.android.Application;
+import de.ladis.infohm.android.widget.ScrollView;
+import de.ladis.infohm.android.widget.ScrollView.OnScrollableListener;
 import de.ladis.infohm.util.Injector;
 
 public abstract class BaseDialogFragment extends DialogFragment implements Injector {
 
 	private TextView titleView;
-	private ViewGroup contentView;
+	private ScrollView contentView;
+	private View dividerView;
+	private Button positiveButton, negativeButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,7 +42,10 @@ public abstract class BaseDialogFragment extends DialogFragment implements Injec
 		View view = View.inflate(getContext(), R.layout.fragment_dialog_base, null);
 
 		titleView = (TextView) view.findViewById(R.id.fragment_dialog_base_title);
-		contentView = (ViewGroup) view.findViewById(R.id.fragment_dialog_base_content);
+		contentView = (ScrollView) view.findViewById(R.id.fragment_dialog_base_content);
+		dividerView = view.findViewById(R.id.fragment_dialog_base_divider);
+		positiveButton = (Button) view.findViewById(R.id.fragment_dialog_base_actions_positive);
+		negativeButton = (Button) view.findViewById(R.id.fragment_dialog_base_actions_negative);
 
 		return view;
 	}
@@ -49,16 +57,31 @@ public abstract class BaseDialogFragment extends DialogFragment implements Injec
 		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		ViewGroup container = contentView;
 
-		View dialog = onCreateDialogView(inflater, container, savedInstanceState);
+		View dialog = onCreateDialogContentView(inflater, container, savedInstanceState);
 		container.addView(dialog);
+
+		onDialogTitleViewCreated(titleView);
+		onDialogActionViewCreated(positiveButton, negativeButton);
 
 		ButterKnife.inject(this, view);
 	}
 
-	public abstract View onCreateDialogView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState);
+	public abstract void onDialogTitleViewCreated(TextView title);
 
-	public final void setTitle(int titleResId) {
-		titleView.setText(titleResId);
+	public abstract View onCreateDialogContentView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState);
+
+	public abstract void onDialogActionViewCreated(Button positive, Button negative);
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		contentView.setOnScrollableListener(new OnScrollableListener() {
+			@Override
+			public void onCanScroll(ScrollView view, boolean scrollable) {
+				dividerView.setVisibility(scrollable ? View.VISIBLE : View.GONE);
+			}
+		});
 	}
 
 	public final Context getContext() {
@@ -69,6 +92,13 @@ public abstract class BaseDialogFragment extends DialogFragment implements Injec
 	public void inject(Object object) {
 		Application app = (Application) getActivity().getApplication();
 		app.inject(object);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		contentView.setOnScrollableListener(null);
 	}
 
 }

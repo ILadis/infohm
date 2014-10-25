f32dbe24f5b2a3368048bc317aafd9f7df776e84
Michael Wagner <mwagn4@gmail.com>
2014-10-26 00:58:53 +0200

    Introduces DaoException to be used by dao implementations
    DaoException will be used by dao implementations that my throw
    exceptions on their CRUD operations. This will take together all the
    diffrent exceptions that may occur when invoking diffrent dao
    implementations.

--- infohm/src/main/java/de/ladis/infohm/core/dao/Dao.java
+++ infohm/src/main/java/de/ladis/infohm/core/dao/Dao.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public interface Dao<K, E> {
 
-	public E find(K key);
+	public E find(K key) throws DaoException;
 
-	public List<E> list();
+	public List<E> list() throws DaoException;
 
-	public void insert(E entity);
+	public void insert(E entity) throws DaoException;
 
-	public void update(E entity);
+	public void update(E entity) throws DaoException;
 
-	public void delete(E entity);
+	public void delete(E entity) throws DaoException;
 
 }

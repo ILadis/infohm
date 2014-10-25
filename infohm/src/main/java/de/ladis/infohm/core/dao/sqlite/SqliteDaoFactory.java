115630b6f8855cde528c518d83d4a562a5208d29
Michael Wagner <mwagn4@gmail.com>
2014-10-26 00:54:22 +0200

    Moves SqliteDaoFactory implementation to use android's SQLiteOpenHelper
    Removed stub SqliteHandler implementation of a SQLiteOpenHelper and
    changed SqliteDaoFactory to use this SQLiteOpenHelper in its
    constructor.

--- infohm/src/main/java/de/ladis/infohm/core/dao/sqlite/SqliteDaoFactory.java
+++ infohm/src/main/java/de/ladis/infohm/core/dao/sqlite/SqliteDaoFactory.java
@@ -1,25 +1,26 @@
 package de.ladis.infohm.core.dao.sqlite;
 
+import android.database.sqlite.SQLiteOpenHelper;
 import de.ladis.infohm.core.dao.Dao;
 import de.ladis.infohm.core.dao.DaoFactory;
 
 public class SqliteDaoFactory extends DaoFactory {
 
-	private final SqliteHandler handler;
+	private final SQLiteOpenHelper helper;
 
-	public SqliteDaoFactory(SqliteHandler handler) {
-		this.handler = handler;
+	public SqliteDaoFactory(SQLiteOpenHelper handler) {
+		this.helper = handler;
 	}
 
 	@Override
 	public <T extends Dao<?, ?>> T create(Class<T> dao, Object... arguments) {
-		return getInstanceOf(dao, argumentsWithHandler(arguments));
+		return getInstanceOf(dao, argumentsWithDatabase(arguments));
 	}
 
-	private Object[] argumentsWithHandler(Object... arguments) {
+	private Object[] argumentsWithDatabase(Object... arguments) {
 		Object[] withHandler = new Object[1 + arguments.length];
 
-		withHandler[0] = handler.getWritableDatabase();
+		withHandler[0] = helper.getWritableDatabase();
 		System.arraycopy(arguments, 0, withHandler, 1, arguments.length);
 
 		return withHandler;

86b2bde871ea3ee90ea3d89483c61f39acf15ec8
Michael Wagner <mwagn4@gmail.com>
2014-10-27 15:45:22 +0100

    Adds first entity objects
    Added Bookmark and Publisher objects that we'll definitely need later.

---
+++ infohm/src/main/java/de/ladis/infohm/core/domain/Bookmark.java
@@ -0,0 +1,42 @@
+package de.ladis.infohm.core.domain;
+
+public class Bookmark {
+
+	private Long id;
+	private String url;
+	private String title;
+	private String description;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+}
---
+++ infohm/src/main/java/de/ladis/infohm/core/domain/Publisher.java
@@ -0,0 +1,33 @@
+package de.ladis.infohm.core.domain;
+
+public class Publisher {
+
+	private Long id;
+	private String name;
+	private String description;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+}

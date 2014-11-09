92b60b6f601cda2274402449819cc37d10f79478
Michael Wagner <mwagn4@gmail.com>
2014-11-09 21:50:13 +0100

    Adds android SQLiteOpenHelper class to create the cache database
    First implementation of an android SQLiteOpenHelper class that will
    create the sqlite database and also setup all tables. This
    implementation will create two tables for both kind of entities
    (Bookmark and Publisher).

---
+++ infohm/src/main/java/de/ladis/infohm/core/dao/sqlite/helper/SqliteOpenHelperV1.java
@@ -0,0 +1,23 @@
+package de.ladis.infohm.core.dao.sqlite.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqliteOpenHelperV1 extends SQLiteOpenHelper {
+
+	public SqliteOpenHelperV1(Context context) {
+		super(context, "infohm", null, 1);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE bookmark (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT NOT NULL, title TEXT NOT NULL, description TEXT NOT NULL)");
+		db.execSQL("CREATE TABLE publisher (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT NOT NULL)");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	}
+
+}

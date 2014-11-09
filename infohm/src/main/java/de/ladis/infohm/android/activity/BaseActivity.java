19ed9f103284873eee67b27d9c0ca42752eaed50
Michael Wagner <mwagn4@gmail.com>
2014-11-09 16:43:46 +0100

    Extends base activity implementation to support toolbars and drawers
    BaseActivity now finds a toolbar with a pre defined id and automatically
    uses it as the default actionbar if present.
    Added BaseDrawerActivity that finds a navigation drawer with a pre
    defined id and configures it (toggle animation, back button behaviour).
    Updated themes and styles for the navigation drawer.

--- infohm/src/main/java/de/ladis/infohm/android/activity/BaseActivity.java
+++ infohm/src/main/java/de/ladis/infohm/android/activity/BaseActivity.java
@@ -2,13 +2,17 @@ package de.ladis.infohm.android.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import butterknife.ButterKnife;
+import de.ladis.infohm.R;
 import de.ladis.infohm.android.Application;
 import de.ladis.infohm.util.Injector;
 
 public abstract class BaseActivity extends ActionBarActivity implements Injector {
+
+	private Toolbar toolbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +23,34 @@ public abstract class BaseActivity extends ActionBarActivity implements Injector
 	@Override
 	public void setContentView(int layoutResId) {
 		super.setContentView(layoutResId);
-		ButterKnife.inject(this);
+		setContentView();
 	}
 
 	@Override
 	public void setContentView(View view) {
 		super.setContentView(view);
-		ButterKnife.inject(this);
+		setContentView();
 	}
 
 	@Override
 	public void setContentView(View view, LayoutParams params) {
 		super.setContentView(view, params);
+		setContentView();
+	}
+
+	private final void setContentView() {
+		// find and set toolbar
+		toolbar = (Toolbar) findViewById(R.id.activity_toolbar);
+		if (toolbar != null) {
+			setSupportActionBar(toolbar);
+		}
+
+		// inject views
 		ButterKnife.inject(this);
+	}
+
+	public final Toolbar getSupportToolbar() {
+		return toolbar;
 	}
 
 	@Override

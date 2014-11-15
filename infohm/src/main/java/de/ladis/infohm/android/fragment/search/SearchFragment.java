d8682d2bd2ad356caaacdc973834f02a48d91974
Michael Wagner <mwagn4@gmail.com>
2014-11-15 16:46:47 +0100

    Adds search fragment and resources
    If an layout includes this fragment it will cause the toolbar to display
    a search menu.

---
+++ infohm/src/main/java/de/ladis/infohm/android/fragment/search/SearchFragment.java
@@ -0,0 +1,30 @@
+package de.ladis.infohm.android.fragment.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import de.ladis.infohm.R;
+import de.ladis.infohm.android.fragment.BaseFragment;
+
+public class SearchFragment extends BaseFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return new View(inflater.getContext());
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.fragment_search, menu);
+	}
+
+}
--- infohm/src/main/java/de/ladis/infohm/android/module/FragmentModule.java
+++ infohm/src/main/java/de/ladis/infohm/android/module/FragmentModule.java
@@ -2,16 +2,20 @@ package de.ladis.infohm.android.module;
 
 import dagger.Module;
 import de.ladis.infohm.android.fragment.authentication.AuthenticationFragment;
+import de.ladis.infohm.android.fragment.drawer.NavigationDrawerFragment;
+import de.ladis.infohm.android.fragment.events.EventsFragment;
 import de.ladis.infohm.android.fragment.publisher.StarPublisherFragment;
+import de.ladis.infohm.android.fragment.search.SearchFragment;
 
 @Module(
 includes = {
 		AndroidModule.class,
-		ServiceModule.class
+		ServiceModule.class,
 },
 injects = {
 		AuthenticationFragment.class,
-		StarPublisherFragment.class
+		StarPublisherFragment.class,
+		SearchFragment.class,
 })
 public class FragmentModule {
 }

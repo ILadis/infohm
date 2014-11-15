d65fec4675460136a314cb17606484b4566c00af
Michael Wagner <mwagn4@gmail.com>
2014-11-15 16:49:02 +0100

    Adds sliding tab classes, resources and styles

---
+++ infohm/src/main/java/de/ladis/infohm/android/widget/SlidingTabLayout.java
@@ -0,0 +1,191 @@
+package de.ladis.infohm.android.widget;
+
+import de.ladis.infohm.R;
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.TextView;
+
+public class SlidingTabLayout extends HorizontalScrollView {
+
+	private ViewPager viewPager;
+	private ViewPager.OnPageChangeListener pageChangeListener;
+
+	private final int titleOffset;
+	private final SlidingTabStrip tabStrip;
+
+	public SlidingTabLayout(Context context) {
+		this(context, null);
+	}
+
+	public SlidingTabLayout(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public SlidingTabLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+
+		setHorizontalScrollBarEnabled(false);
+		setFillViewport(true);
+
+		titleOffset = (int) (24 * getResources().getDisplayMetrics().density);
+		tabStrip = new SlidingTabStrip(context);
+
+		addView(tabStrip, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+	}
+
+	public void setOnPageChangeListener(ViewPager.OnPageChangeListener listener) {
+		pageChangeListener = listener;
+	}
+
+	public void setViewPager(ViewPager pager) {
+		tabStrip.removeAllViews();
+
+		viewPager = pager;
+		if (viewPager != null) {
+			viewPager.setOnPageChangeListener(new InternalViewPagerListener());
+			viewPager.getAdapter().registerDataSetObserver(new AdapterObserver());
+
+			populateTabStrip();
+		}
+	}
+
+	protected TextView createTabView(Context context) {
+		TextView tabView = new TextView(context, null, R.attr.slidingTabTextViewStyle);
+
+		return tabView;
+	}
+
+	private void populateTabStrip() {
+		tabStrip.removeAllViews();
+
+		final PagerAdapter adapter = viewPager.getAdapter();
+		final View.OnClickListener tabClickListener = new TabClickListener();
+
+		for (int i = 0; i < adapter.getCount(); i++) {
+			TextView tabView = createTabView(getContext());
+
+			tabView.setText(adapter.getPageTitle(i));
+			tabView.setOnClickListener(tabClickListener);
+
+			tabStrip.addView(tabView);
+		}
+	}
+
+	@Override
+	protected void onAttachedToWindow() {
+		super.onAttachedToWindow();
+
+		if (viewPager != null) {
+			scrollToTab(viewPager.getCurrentItem(), 0);
+		}
+	}
+
+	private void scrollToTab(int tabIndex, int positionOffset) {
+		final int tabStripChildCount = tabStrip.getChildCount();
+
+		if (tabStripChildCount == 0 || tabIndex < 0 || tabIndex >= tabStripChildCount) {
+			return;
+		}
+
+		View selectedChild = tabStrip.getChildAt(tabIndex);
+		if (selectedChild != null) {
+			int targetScrollX = selectedChild.getLeft() + positionOffset;
+
+			if (tabIndex > 0 || positionOffset > 0) {
+				targetScrollX -= titleOffset;
+			}
+
+			scrollTo(targetScrollX, 0);
+		}
+	}
+
+	private class InternalViewPagerListener implements ViewPager.OnPageChangeListener {
+		private int scrollState;
+
+		@Override
+		public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+			int tabStripChildCount = tabStrip.getChildCount();
+
+			if ((tabStripChildCount == 0) || (position < 0) || (position >= tabStripChildCount)) {
+				return;
+			}
+
+			tabStrip.onViewPagerPageChanged(position, positionOffset);
+
+			View selectedTitle = tabStrip.getChildAt(position);
+			int extraOffset = (selectedTitle != null) ? (int) (positionOffset * selectedTitle.getWidth()) : 0;
+			scrollToTab(position, extraOffset);
+
+			if (pageChangeListener != null) {
+				pageChangeListener.onPageScrolled(position, positionOffset, positionOffsetPixels);
+			}
+
+			if (positionOffset <= 0.01f) {
+				for (int i = 0; i < tabStrip.getChildCount(); i++) {
+					View childView = tabStrip.getChildAt(i);
+
+					if (TextView.class.isInstance(childView)) {
+						TextView tabView = (TextView) childView;
+						tabView.setActivated(i == position);
+					}
+				}
+			}
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int state) {
+			scrollState = state;
+
+			if (pageChangeListener != null) {
+				pageChangeListener.onPageScrollStateChanged(state);
+			}
+		}
+
+		@Override
+		public void onPageSelected(int position) {
+			if (scrollState == ViewPager.SCROLL_STATE_IDLE) {
+				tabStrip.onViewPagerPageChanged(position, 0f);
+				scrollToTab(position, 0);
+			}
+
+			if (pageChangeListener != null) {
+				pageChangeListener.onPageSelected(position);
+			}
+		}
+
+	}
+
+	private class TabClickListener implements View.OnClickListener {
+
+		@Override
+		public void onClick(View view) {
+			for (int i = 0; i < tabStrip.getChildCount(); i++) {
+				if (view == tabStrip.getChildAt(i)) {
+					if (TextView.class.isInstance(view)) {
+						TextView tabView = (TextView) view;
+						tabView.setActivated(true);
+					}
+
+					viewPager.setCurrentItem(i);
+					return;
+				}
+			}
+		}
+
+	}
+
+	private class AdapterObserver extends DataSetObserver {
+
+		@Override
+		public void onChanged() {
+			populateTabStrip();
+		}
+
+	}
+
+}

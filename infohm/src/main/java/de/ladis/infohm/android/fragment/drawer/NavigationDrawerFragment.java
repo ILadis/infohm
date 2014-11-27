1f2ede0b04d0957f2aac5515ac40b838c0555944
Michael Wagner <mwagn4@gmail.com>
2014-11-27 21:20:05 +0100

    Rough implementation for the navigation drawer fragment

--- infohm/src/main/java/de/ladis/infohm/android/fragment/drawer/NavigationDrawerFragment.java
+++ infohm/src/main/java/de/ladis/infohm/android/fragment/drawer/NavigationDrawerFragment.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import de.ladis.infohm.R;
 import de.ladis.infohm.android.fragment.BaseFragment;
 
 public class NavigationDrawerFragment extends BaseFragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return new View(inflater.getContext());
+		return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
 	}
 
 }

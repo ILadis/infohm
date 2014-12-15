6cfda99319836205c1cac9404e1be3e3a6cbeaee
Michael Wagner <mwagn4@gmail.com>
2014-12-15 13:09:41 +0100

    Adds hamcrest-all to pom and updated validators with new matchers

--- infohm/src/main/java/de/ladis/infohm/android/validation/UsernameValidator.java
+++ infohm/src/main/java/de/ladis/infohm/android/validation/UsernameValidator.java
@@ -1,8 +1,6 @@
 package de.ladis.infohm.android.validation;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.mockito.AdditionalMatchers.*;
-import static org.mockito.Matchers.*;
+import static org.hamcrest.Matchers.*;
 
 import de.ladis.infohm.core.validation.Validator;
 
@@ -11,8 +9,8 @@ public class UsernameValidator extends Validator<String> {
 	@Override
 	public void validate(String username) {
 		if (!"rest".equals(username)) {
-			validateThat(username, is(notNull()));
-			validateThat(username.length(), is(gt(16)));
+			validateThat(username, is(notNullValue()));
+			validateThat(username.length(), is(greaterThan(16)));
 		}
 	}
 
